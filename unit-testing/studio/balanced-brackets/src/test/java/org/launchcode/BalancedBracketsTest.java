8008c9bd572ab6f4fc7f3cb6224c6c773d640d7d
jmalone487 <jmalone487@gmail.com>
2024-09-23 18:50:49 -0500

    Your commit message

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -1,13 +1,66 @@
 package org.launchcode;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
-    //TODO: add tests here
+
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertTrue(true);
+    }
+
+    // Test for a string with no brackets (should be considered balanced)
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    // Test for a balanced pair of brackets
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    // Test for a string with balanced brackets around text
+    @Test
+    public void bracketsAroundTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    // Test for an unbalanced pair of brackets (opening but no closing)
+    @Test
+    public void onlyOpeningBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    // Test for an unbalanced pair of brackets (closing but no opening)
+    @Test
+    public void onlyClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
+    }
+
+    // Test for balanced brackets with multiple pairs
+    @Test
+    public void multipleBalancedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch][Code]"));
+    }
+
+    // Test for unbalanced brackets with multiple pairs
+    @Test
+    public void unbalancedBracketsWithMultiplePairsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch][Code"));
+    }
+
+    // Test for nested balanced brackets
+    @Test
+    public void nestedBalancedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]]"));
+    }
+
+    // Test for unbalanced nested brackets
+    @Test
+    public void unbalancedNestedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]"));
     }
 }
